5be6e11d5eeed347afcebbb8a9bd2632afabf979
nightcrawler086 <brian.hall4@gmail.com>
2024-11-19 22:15:35 -0500

    change stream is working

--- cmd/mongo_cs/main.go
+++ cmd/mongo_cs/main.go
@@ -24,28 +24,32 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	mongoUri := conf.MongoDBString
-	db, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	defer func() {
-		if err = db.Disconnect(context.TODO()); err != nil {
+		if err = client.Disconnect(context.TODO()); err != nil {
 			panic(err)
 		}
 	}()
 	var result bson.M
-	if err := db.Database("mongo_cs").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
+	if err := client.Database("mongo_cs").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
 		panic(err)
 	}
 	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
-	col := db.Database("mongo_cs").Collection("inventory")
-	cs, err := col.Watch(ctx, mongo.Pipeline{})
+	col := client.Database("mongo_cs").Collection("inventory")
+	cs, err := col.Watch(context.TODO(), mongo.Pipeline{})
 	if err != nil {
 		panic(err)
 	}
-	defer cs.Close(ctx)
-	ok := cs.Next(ctx)
-	fmt.Println(ok)
-	next := cs.Current
-	fmt.Println(next)
+	defer cs.Close(context.TODO())
+
+	for cs.Next(context.TODO()) {
+		var data bson.M
+		if err := cs.Decode(&data); err != nil {
+			panic(err)
+		}
+		fmt.Println(data)
+	}
 }
